2131242267855df051fca0faff810d3920c9f044
rostun <rosa.m.tung@gmail.com>
2016-02-20 21:06:44 -0500

    query descriptions
    more queries

--- generalUseQueries.sql
+++ generalUseQueries.sql
@@ -18,45 +18,10 @@ SELECT * FROM lol_champions;
 SELECT * FROM lol_factions;
 SELECT * FROM lol_regions;
 
-
-/* * * * * * * * * * * * * * * * * * * *
- Display Champions Names and Races
- order by: release date
- * * * * * * * * * * * * * * * * * * * */
-
-SELECT 	lol_champions.name AS "Champion Name", 
-		lol_races.name AS "Race" , 
-		lol_champions.releaseDate AS "Release Date"
-FROM lol_champions
-INNER JOIN lol_races ON lol_champions.race_id = lol_races.race_id  
-ORDER BY releaseDate ASC;
-
-/* * * * * * * * * * * * * * * * * * * *
- Display Races + # Champions per Race
- order by: alphabetical order
- * * * * * * * * * * * * * * * * * * * */
-SELECT 	lol_races.name AS "Race", 
-		COUNT(lol_champions.race_id) AS "Number of Champions"
-FROM lol_races 
-INNER JOIN lol_champions ON lol_champions.race_id = lol_races.race_id
-GROUP BY lol_races.name
-ORDER BY lol_races.name ASC;
- 
-/* * * * * * * * * * * * * * * * * * * *
- Champions with [occupation]
- * * * * * * * * * * * * * * * * * * * */
-
-/* * * * * * * * * * * * * * * * * * * *
- Champion Romantic Connections 
- * * * * * * * * * * * * * * * * * * * */
-
-/* * * * * * * * * * * * * * * * * * * *
- Champion not loyal to starting faction
- * * * * * * * * * * * * * * * * * * * */
- 
 /* * * * * * * * * * * * * * * * * * * *
  Champion Details
- minus allegiances and occupations 
+	names, genders, races, origin, and
+	release dates
  order by alphabetical order
  * * * * * * * * * * * * * * * * * * * */
 SELECT 	lol_champions.name AS "Champion",
@@ -69,10 +34,60 @@ LEFT JOIN lol_races ON lol_champions.race_id = lol_races.race_id
 LEFT JOIN lol_factions ON lol_champions.birth_faction_id = lol_factions.faction_id
 lEFT JOIN lol_regions ON lol_champions.birth_region_id = lol_regions.region_id
 ORDER BY lol_champions.name ASC;
+
+/* * * * * * * * * * * * * * * * * * * *
+ Display Allegiances + # Champions per
+ order by: alphabetical order
+ * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * *
+ Display Occupations + # Champions per
+ order by: alphabetical order
+ * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * *
+ Display Races + # Champions per 
+ order by: alphabetical order
+ * * * * * * * * * * * * * * * * * * * */
+SELECT 	lol_races.name AS "Race", 
+		COUNT(lol_champions.race_id) AS "Number of Champions"
+FROM lol_races 
+LEFT JOIN lol_champions ON lol_champions.race_id = lol_races.race_id
+GROUP BY lol_races.name
+ORDER BY lol_races.name ASC;
+
+ /* * * * * * * * * * * * * * * * * * * *
+ Display Champions Names and Races
+ order by: release date
+ * * * * * * * * * * * * * * * * * * * */
+SELECT 	lol_champions.name AS "Champion Name", 
+		lol_races.name AS "Race" , 
+		lol_champions.releaseDate AS "Release Date"
+FROM lol_champions
+LEFT JOIN lol_races ON lol_champions.race_id = lol_races.race_id  
+ORDER BY releaseDate ASC;
+
+/* * * * * * * * * * * * * * * * * * * * * *
+ Display Champions in Romantic Relationships
+ order by: alphabetical order
+ * * * * * * * * * * * * * * * * * * * * * */
  
- 
- 
- 
+/* * * * * * * * * * * * * * * * * * * * * *
+ Display Champions who are related
+ order by: alphabetical order
+ * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * *
+ Display Champions who are allies
+ order by: alphabetical order
+ * * * * * * * * * * * * * * * * * * * * * */
+
+/* * * * * * * * * * * * * * * * * * * * * *
+ Display Champions who are enemies
+ order by: alphabetical order
+ * * * * * * * * * * * * * * * * * * * * * */
+
+
  
  
  
