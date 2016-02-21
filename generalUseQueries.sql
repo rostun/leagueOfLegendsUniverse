870eeaec77943a89f39ee06473f42db05d1c2a5f
rostun <rosa.m.tung@gmail.com>
2016-02-20 19:29:42 -0500

    add general use queries
    champion names and races
    races and #champions per race

--- generalUseQueries.sql
+++ generalUseQueries.sql
@@ -23,14 +23,22 @@ SELECT * FROM lol_regions;
  order by: release date
  * * * * * * * * * * * * * * * * * * * */
 
-SELECT lol_champions.name AS "Champion Name", lol_races.name AS "Race" , lol_champions.releaseDate AS "Release Date"
+SELECT 	lol_champions.name AS "Champion Name", 
+		lol_races.name AS "Race" , 
+		lol_champions.releaseDate AS "Release Date"
 FROM lol_champions
 INNER JOIN lol_races ON lol_champions.race_id = lol_races.race_id  
 ORDER BY releaseDate ASC;
 
 /* * * * * * * * * * * * * * * * * * * *
- Count # Champions per Race
+ Display Races + # Champions per Race
+ order by: alphabetical order
  * * * * * * * * * * * * * * * * * * * */
+SELECT 	lol_races.name AS "Race", 
+		COUNT(lol_champions.race_id) AS "Number of Champions"
+FROM lol_races 
+INNER JOIN lol_champions ON lol_champions.race_id = lol_races.race_id
+GROUP BY lol_races.name;
  
 /* * * * * * * * * * * * * * * * * * * *
  Champions with [occupation]
